511accbfecf5e58e8ef191744f183898dc9628a5
jxiao <jxiao@thoughtworks.com>
2021-02-03 20:55:00 +0800

    [xj] bitmap

--- src/main/java/jvm/valatile/VolatileTest.java
+++ src/main/java/jvm/valatile/VolatileTest.java
@@ -1,41 +1,25 @@
 package jvm.valatile;
 
-import sun.misc.Contended;
-
 public class VolatileTest extends Thread {
-    @Contended
-//    int flag = 0;
-    volatile Integer flag = 0;
-    //    Boolean flag = false;
+    boolean flag = true;
+    long count = 0L;
 
-    //            Integer i = 0;
-    long i = 0L;
-
+    @Override
     public void run() {
-//        System.out.println("Begin - " + Thread.currentThread().getName() + " flag hashcode:" + flag.hashCode());
-        while (flag == 0) {
-            i++;
-
-
-//            if (i == 100) {
-//                System.out.println(Thread.currentThread().getName() + " flag is:" + flag);
-//            }
-//
-//            if (i == 3793651106L) {
-////                System.out.println(Thread.currentThread().getName() + " flag is:" + flag);
-//                break;
-//            }
-
-//            try {
-//                Thread.sleep(10);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+        while (flag) {
+            if (count % 4300000000L == 0 && count != 0L) {
+                sleepThread();
+            }
+            count++;
         }
-//        System.out.println(Thread.currentThread().getName() + " child out. i:" + i);
-//        if (flag == 1) {
-//            System.out.println(Thread.currentThread().getName() + " flag is:" + flag);
-//        }
+    }
+
+    private static void sleepThread() {
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -43,14 +27,12 @@ public class VolatileTest extends Thread {
         vt.start();
         Thread.sleep(1000);
         setFlag(vt);
-        System.out.println("stop " + vt.i);
-        System.out.println(Thread.currentThread().getName() + " flag hashcode:" + vt.flag.hashCode());
         vt.join();
-        System.out.println("flag " + vt.flag);
-//        System.out.println(ClassLayout.parseInstance(vt).toPrintable());
+        System.out.println("vt value: " + vt.count);
+        System.out.println("stop and final flag is:" + vt.flag);
     }
 
     private static void setFlag(VolatileTest vt) {
-        vt.flag = 1;
+        vt.flag = false;
     }
 }
