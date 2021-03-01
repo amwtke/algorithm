6030550fcd5ec5df7603716fe2de0cc217a8112e
jxiao <jxiao@thoughtworks.com>
2021-03-01 17:34:54 +0800

    [xj] multicore multithread class

--- src/main/java/jvm/mulitcore_mulitthread_programming/hsdis/VolatileNotOk.java
+++ src/main/java/jvm/mulitcore_mulitthread_programming/hsdis/VolatileNotOk.java
@@ -6,6 +6,7 @@ public class VolatileNotOk extends Thread {
 
     public static void main(String[] args) throws Exception {
         VolatileNotOk vt = new VolatileNotOk();
+        System.out.println(vt.hashCode());
         vt.start();
         Thread.sleep(1000);//挂起下主线程，确保子线程完成初始化并启动成功。
         setFlag(vt);//设置flag，尝试停止线程
@@ -16,12 +17,17 @@ public class VolatileNotOk extends Thread {
 
     @Override
     public void run() {
-        while (flag) {
+        System.out.println(this.hashCode());
+        while (getFlag(this)) {
             i++;
         }
     }
 
     private static void setFlag(VolatileNotOk vt) {
         vt.flag = false;
+    }
+
+    private static boolean getFlag(VolatileNotOk vt) {
+        return vt.flag;
     }
 }
