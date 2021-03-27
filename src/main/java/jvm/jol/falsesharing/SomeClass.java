3db90a3e9d6fb34f5a8b8fa1ead6d5eb2bfbc656
jxiao <jxiao@thoughtworks.com>
2021-03-27 21:00:03 +0800

    [xj] multicore multithread class

--- src/main/java/jvm/jol/falsesharing/SomeClass.java
+++ src/main/java/jvm/jol/falsesharing/SomeClass.java
@@ -2,13 +2,16 @@ package jvm.jol.falsesharing;
 
 import org.openjdk.jol.info.ClassLayout;
 import org.openjdk.jol.vm.VM;
+import sun.misc.Contended;
 
 public class SomeClass {
+    @Contended
     public volatile long valueA;
     public volatile long valueB;
 
     public static void main(String[] args){
+        SomeClass instance = new SomeClass();
         System.out.println(VM.current().details());
-        System.out.println(ClassLayout.parseInstance(new SomeClass()).toPrintable());
+        System.out.println(ClassLayout.parseInstance(instance).toPrintable());
     }
 }
