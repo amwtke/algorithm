1af6da92092dc72d088acd724640c6dd61d9105a
jin.xiao <jin.xiao@rea-group.com>
2021-07-26 18:28:24 +0800

    just test git

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,10 @@
 import org.junit.Test;
 
 public class test {
+    /**
+     * just test git ignor
+     * @param arges
+     */
     public static void main(String[] arges) {
         System.out.println("hh");
     }
